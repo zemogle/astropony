f9bbe023044d406d7ff21cf72a4acbf3e7a1dc7f
Edward Gomez <edward@gomez.me.uk>
2016-06-14 10:49:43 +0100

    updateing db and templates

--- exoplanets/views.py
+++ exoplanets/views.py
@@ -5,17 +5,18 @@ from exoplanets.models import Planet
 from .models import *
 
 def home(request):
-	no_exoplanets = Planet.objects.all().count()
-	return render(request,'exoplanets/index.html',{'no_exoplanets': no_exoplanets})
+    no_exoplanets = Planet.objects.all().count()
+    return render(request,'exoplanets/index.html',{'no_exoplanets': no_exoplanets})
 
 def planets(request):
-	planets = Planet.objects.all()
-	return render(request,'exoplanets/planets.html',{'planets':planets})
+    planets = Planet.objects.all()
+    return render(request,'exoplanets/planets.html',{'planets':planets})
 
 class PlanetUpdate(UpdateView):
     model = Planet
     fields = ['name']
     template_name = 'exoplanets/planet_update.html'
+    success_url = '/'
 
 class PlanetDetails(DetailView):
     model = Planet
